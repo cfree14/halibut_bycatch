0c373086d80d60ba22671c1246d2f311b922fc86
Chris Free <cfree@Chriss-MacBook-Pro-2.local>
2022-03-26 14:44:48 -0700

    data work

--- data/gillnet_logbooks/Step1_format_cdfw_gillnet_logbook_data.R
+++ data/gillnet_logbooks/Step1_format_cdfw_gillnet_logbook_data.R
@@ -22,15 +22,53 @@ data_orig <- readxl::read_excel(file.path(indir, "GillnetLogs_2000-2020.xlsx"))
 # Format data
 ################################################################################
 
-# Needs a ton of work
-
+# Can fill in gaps in vessel info
+# Can fix up species info
+# Look at keys
 
 # Format data
 data <- data_orig %>%
   # Rename
   janitor::clean_names("snake") %>%
   rename(vessel=vessel_name,
-         target_spp_code=tarspc)
+         fg_id=fish_game_receipt_no,
+         boat_id=boatno,
+         date=fishing_date,
+         target_spp_code=tarspc,
+         soak_duration_hr=hours_net_soaked,
+         block_id=fg_blocks,
+         depth_fa=depths,
+         net_length_ft=net_length,
+         mesh_size_in=mesh_size,
+         line_depth_fa=bouy_line_depth,
+         species=common_name,
+         species_code=mlds_species_code,
+         catch_n=num_catch,
+         catch_lb=weights) %>%
+  # Format status
+  mutate(status=tolower(status)) %>%
+  # Format predator
+  mutate(predator=predator %>% gsub("_", " ", .) %>% stringr::str_to_sentence(.),
+         predator=recode(predator,
+                         "Harbor"="Harbor seal",
+                         "Mako"="Mako shark",
+                         "Sea lion - hagfish"="Sea lion, hagfish",
+                         "Seals"="Seal"))
 
 # Inspect
 str(data)
+freeR::complete(data)
+
+# Inspect
+range(data$date, na.rm=T)
+table(data$drift_set)
+table(data$status)
+table(data$predator)
+
+
+# Export data
+################################################################################
+
+# Export
+saveRDS(data, file=file.path(outdir, "2000_2020_gillnet_logbook_data.Rds"))
+
--- data/trawl_logbooks/Step1_format_cdfw_trawl_logbook_data.R
+++ data/trawl_logbooks/Step1_format_cdfw_trawl_logbook_data.R
@@ -16,19 +16,95 @@ plotdir <- "data/trawl_logbooks/figures"
 
 # Read data
 list.files(indir)
-data_orig <- read.csv(file.path(indir, "MLS_TrawlExtract_03182022.csv"))
+data_orig <- read.csv(file.path(indir, "MLS_TrawlExtract_03182022.csv"), na.strings = "")
 
+
+
+# Helper function
+################################################################################
+
+# Function to collapse repeated dates
+collapse_dates <- function(x){
+  # Break and unique
+  z <- purrr::map_chr(x, function(x){
+    y <- str_split(x, pattern=", ", simplify = T) %>% as.character() %>% unique()
+  })
+  return(z)
+}
+collapse_dates(x=c("2010-02-11, 2010-02-11", "2010-02-18, 2010-02-18, 2010-02-18"))
 
 # Format data
 ################################################################################
 
-# Needs a ton of work
-
-
 # Format data
 data <- data_orig %>%
   # Rename
-  janitor::clean_names("snake")
+  janitor::clean_names("snake") %>%
+  rename(vessel=vessel_name,
+         return_date=return_date_string,
+         set_lat_dd=set_latitude,
+         set_long_dd=set_longitude,
+         up_lat_dd=up_latitude,
+         up_long_dd=up_longitude,
+         depth_avg_fa=average_depth,
+         catch_lb=pounds,
+         catch_lb_conv=converted_pounds,
+         spp_code=species_code,
+         pacfin_code=pac_fin_species_code,
+         value_usd=revenue,
+         efp_trip_yn=is_efp_trip,
+         observed_trip_yn=is_observed_trip) %>%
+  # Convert return/departure dates
+  mutate(departure_date=lubridate::mdy(departure_date),
+         return_date=lubridate::mdy(return_date)) %>%
+  # Build tow date
+  mutate(tow_date=paste(tow_year, tow_month, tow_day, sep="-") %>% lubridate::ymd()) %>%
+  # Fix and convert landing dates
+  mutate(landing_date=collapse_dates(x=landing_date),
+         landing_date=lubridate::ymd(landing_date)) %>%
+  # Fix longtidues
+  mutate(set_long_dd=set_long_dd*-1,
+         up_long_dd=up_long_dd*-1) %>%
+  # Arrange
+  select(serial_number:return_port_code,
+         tow_date, tow_month, tow_day, tow_year, everything())
 
 # Inspect
 str(data)
+freeR::complete(data)
+
+# Inspect
+range(data$tow_date)
+table(data$net_type)
+table(data$target_strategy)
+table(data$observed_trip_yn)
+table(data$efp_trip_yn)
+table(data$region)
+
+# World
+usa <- rnaturalearth::ne_states(country="United States of America", returnclass = "sf")
+mexico <- rnaturalearth::ne_countries(country="Mexico", returnclass = "sf")
+
+# Plot points
+ggplot() +
+  # Country
+  geom_sf(data=usa, fill="grey80", color="white", lwd=0.2, inherit.aes=F) +
+  geom_sf(data=mexico, fill="grey80", color="white", lwd=0.2, inherit.aes=F) +
+  # Tows
+  geom_point(data=data, mapping=aes(x=set_long_dd, y=set_lat_dd), size=0.1, color="red") +
+  geom_point(data=data, mapping=aes(x=up_long_dd, y=up_lat_dd), size=0.1, color="blue") +
+  # Labels
+  labs(x="", y="") +
+  # Crop
+  coord_sf(y=c(30, 45), x=c(-110, -130)) +
+  # Theme
+  theme_bw()
+
+
+# Export data
+################################################################################
+
+# Export
+saveRDS(data, file=file.path(outdir, "CDFW_2000_2020_trawl_logbook_data.Rds"))
+
+
