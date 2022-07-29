2d5c1dae34cbde313cd8066ae8d0689ebc3c5661
Chris Free <cfree14@gmail.com>
2022-07-29 10:39:57 -0700

    fed obs gill net work

--- data/swfsc_obs/Step3_analyze_swfsc_obs_data_discards_all.R
+++ data/swfsc_obs/Step3_analyze_swfsc_obs_data_discards_all.R
@@ -10,62 +10,65 @@ rm(list = ls())
 library(tidyverse)
 
 # Directories
-indir <- "data/cdfw_obs/raw"
-outdir <- "data/cdfw_obs/processed"
-plotdir <- "data/cdfw_obs/figures"
-
+indir <- "data/swfsc_obs/raw"
+outdir <- "data/swfsc_obs/processed"
+plotdir <- "data/swfsc_obs/figures"
 
 # Read data
-list.files(outdir)
-metadata_orig <- readRDS(file.path(outdir, "CDFW_1983_1989_gillnet_observer_set_info.Rds"))
-data_orig <- readRDS(file.path(outdir, "CDFW_1983_1989_gillnet_observer_data.Rds"))
+data_obs <- readRDS(file=file.path(outdir, "SWFSC_set_net_observer_data.Rds"))
+data_lengths <- readRDS(file=file.path(outdir, "SWFSC_set_net_observer_length_comps.Rds"))
+data_meta <- readRDS(file=file.path(outdir, "SWFSC_1990_2017_set_net_observer_trips.Rds"))
+
+# Source
+source("code/helper_functions.R")
 
 
 # Build data
 ################################################################################
 
-# Mark whether small or large mesh
-metadata <- metadata_orig %>%
-  # Mark
-  mutate(mesh_type1=ifelse(is.na(mesh_size1_in), "unknown",
-                           ifelse(mesh_size1_in>=8.5, "large", "small")),
-         mesh_type2=ifelse(is.na(mesh_size2_in), "unknown",
-                           ifelse(mesh_size2_in>=8.5, "large", "small")),
-         mesh_type=paste(mesh_type1, mesh_type2, sep="-"))
-table(metadata$mesh_type)
+# Port key
+# Determine whether ports are N/S of Pt Arguello
+port_key_big <- wcfish::ports
+port_key <- tibble(port=sort(unique(c(data_meta$port_depart, data_meta$port_return)))) %>%
+  mutate(port_match=recode(port,
+                           "Newport"="Newport Beach",
+                           "Port San Luis"="Avila/Port San Luis")) %>%
+  left_join(port_key_big %>% select(port, lat_dd, long_dd), by=c("port_match"="port")) %>%
+  mutate(region=ifelse(lat_dd>=34.6, "Northern", "Southern"))
+southern_ports <- port_key %>%
+  filter(region=="Southern") %>%
+  pull(port)
+
 
 # Build data
-data <- data_orig %>%
+data <- data_obs %>%
   # Add set meta data
-  left_join(metadata %>% select(set_id, lat_dd, long_dd, bottom_depth_fa, net_orientation, net_type, target_spp), by="set_id") %>%
+  left_join(data_meta %>% select(set_id, target1_spp, season, date_haul1, port_depart, port_return,  haul_depth_fa), by="set_id") %>%
+  rename(depth_fa=haul_depth_fa, date=date_haul1) %>%
+  # Reduce to post-1994 data
+  filter(season>=1994) %>%
+  # Reduce to SOUTHERN fishery
+  filter(port_depart %in% southern_ports & port_return %in% southern_ports) %>%
   # Reduce to sets targeting CA halibut
-  filter(target_spp=="California halibut" & n_kept >0) %>%
+  filter(target1_spp=="Halibut, California") %>%
   # Add year
   mutate(year=lubridate::year(date),
          month=lubridate::month(date),
          yday=lubridate::yday(date)) %>%
-  # Summarize by set and species
-  # This is b/c some sets will have multiple rows for a species...
-  group_by(year, month, yday, date,
-         vessel_id, set_num, set_id,
-         net_type, net_orientation, bottom_depth_fa, lat_dd, long_dd,
-         spp_code_chr, comm_name) %>%
-  summarize(landings_n=sum(n_kept, na.rm=T)) %>%
-  ungroup() %>%
-  # Halibut bycatch
+  # Record halibut catch and reduce to sets with halibut
   group_by(set_id) %>%
   mutate(halibut_yn="California halibut" %in% comm_name) %>%
   filter(halibut_yn==T) %>%
   ungroup() %>%
   select(-halibut_yn) %>%
   # Calculate bycatch ratios
+  mutate(discards_n=n_returned_alive+n_returned_dead+n_returned_unknown) %>%
   group_by(set_id) %>%
-  mutate(halibut_n=landings_n[comm_name=="California halibut"],
-         ratio=landings_n/halibut_n) %>%
+  mutate(halibut_n=n_kept[comm_name=="California halibut"],
+         ratio=discards_n/halibut_n) %>%
   ungroup() %>%
-  filter(halibut_n>0) %>%
-  # Remove halibut
-  filter(comm_name!="California halibut") %>%
+  # Filter to useable surveys
+  filter(halibut_n>0 & ratio>0) %>%
   # Add date dummy
   mutate(date_dummy=paste("2020", lubridate::month(date), lubridate::day(date), sep="-") %>% lubridate::ymd(.))
 
@@ -81,23 +84,27 @@ freeR::complete(data)
 # Number of sets
 nsets_tot <- n_distinct(data$set_id)
 
-# Build stats
+# Determine species order
 stats <- data %>%
   group_by(comm_name) %>%
   summarize(nsets=n_distinct(set_id),
-            psets=nsets/nsets_tot) %>%
-  ungroup() %>%
+            psets=nsets/nsets_tot,
+            ratio_med=median(ratio)) %>%
   arrange(desc(psets))
 
 # Top 20 species
 top20spp <- stats$comm_name[1:20]
+top40spp <- stats$comm_name[1:40]
 
 
-# Plot data - bycatch species
+# Plot data
 ################################################################################
 
+# Bycatch species
+#######################################
+
 # Theme
-theme1 <-  theme(axis.text=element_text(size=5),
+theme2 <-  theme(axis.text=element_text(size=6),
                  axis.title=element_text(size=7),
                  axis.title.y=element_blank(),
                  plot.title=element_text(size=8),
@@ -110,33 +117,34 @@ theme1 <-  theme(axis.text=element_text(size=5),
                  # Legend
                  legend.background = element_rect(fill=alpha('blue', 0)))
 
-# Plot data
-g1 <- ggplot(stats, aes(y=factor(comm_name, levels=comm_name), x=psets)) +
-  # Boxplots
+
+# Plot bycatch ratio boxplots
+g1 <- ggplot(stats %>% filter(comm_name %in% top40spp), aes(x=psets, y=factor(comm_name, levels=stats$comm_name))) +
   geom_bar(stat="identity") +
+  # Reference line
+  geom_hline(yintercept=20.5, linetype="dashed") +
   # Labels
-  labs(x="Bycatch occurence\n(percent of gillnet sets)", y="", tag="A", title="CDFW gillnet observer data") +
+  labs(x="Catch occurrence\n(percent of gill net sets)", y="", tag="A", title="SWFSC gill net observer data\nsouthern large-mesh fishery") +
   # Axis
   scale_x_continuous(labels=scales::percent) +
   # Theme
-  theme_bw() + theme1
+  theme_bw() + theme2
 g1
 
-# Plot data
-g2 <- ggplot(data, aes(y=factor(comm_name, levels=stats$comm_name), x=ratio)) +
-  # Boxplots
-  geom_boxplot(fill="grey90", lwd=0.2, outlier.size = 0.3) +
+# Plot bycatch ratio boxplots
+g2 <- ggplot(data %>% filter(comm_name %in% top40spp), aes(x=ratio, y=factor(comm_name, levels=top40spp))) +
+  geom_boxplot(lwd=0.2, outlier.size = 0.5, color="grey30", fill="grey90") +
   # Reference line
+  geom_hline(yintercept=20.5, linetype="dashed") +
+  # Vertical line
   geom_vline(xintercept = 1) +
   # Labels
-  labs(x="Bycatch ratio\n(bycatch / halibut catch)", y="", tag="B", title="  ") +
+  labs(x="Catch ratio\n(discards / halibut landings)", y="", tag="B", title=" \n ") +
   # Axis
-  scale_x_continuous(trans="log10",
-                     breaks=c(0.1, 1, 10, 100),
-                     labels=c("0.1", "1", "10", "100")) +
+  scale_x_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
   # Theme
-  theme_bw() + theme1 +
-  theme(axis.text.y=element_blank())
+  theme_bw() + theme2 +
+  theme(axis.text.y = element_blank())
 g2
 
 # Merge plots
@@ -144,16 +152,15 @@ g <- gridExtra::grid.arrange(g1, g2, nrow=1, widths=c(0.55, 0.45))
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_cdfw_gillnet_obs_bycatch_ratio_by_species.png"),
-       width=6.5, height=8.5, units="in", dpi=600)
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_ratio_by_species_southern_large.png"),
+       width=6.5, height=6.5, units="in", dpi=600)
 
 
-
-# Plot data - bycatch over time
-################################################################################
+# Bycatch over time
+#######################################
 
 # Theme
-theme2 <- theme(axis.text=element_text(size=7),
+theme1 <- theme(axis.text=element_text(size=7),
                 axis.title=element_text(size=8),
                 strip.text=element_text(size=7),
                 plot.title=element_text(size=8),
@@ -172,21 +179,23 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=year, y=ratio, group=y
   # Reference line
   geom_hline(yintercept=1) +
   # Labels
-  labs(y="Bycatch ratio\n(bycatch / halibut catch)", x="Year", title="CDFW gillnet observer data") +
+  labs(y="Catch ratio\n(discards / halibut landings)", x="Year", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
-  scale_x_continuous(breaks=1983:1989) +
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
+  scale_x_continuous(breaks = seq(1995,2020,5), lim=c(1993,2020)) +
   # Theme
-  theme_bw() + theme2 +
+  theme_bw() + theme1 +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_cdfw_gillnet_obs_bycatch_ratio_over_time.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_ratio_over_time_southern_large.png"),
        width=6.5, height=5, units="in", dpi=600)
 
-# Plot data - bycatch by driver
-################################################################################
+
+
+# Bycatch by driver
+#######################################
 
 # Theme
 theme3 <- theme(axis.text=element_text(size=7),
@@ -202,14 +211,14 @@ theme3 <- theme(axis.text=element_text(size=7),
                 legend.background = element_rect(fill=alpha('blue', 0)))
 
 # Bycatch ratio by depth
-g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=bottom_depth_fa, y=ratio)) +
-  facet_wrap(~factor(comm_name, levels=top20spp), ncol=5, scales="free") +
-  geom_smooth(fill="grey80", color="black") +
-  geom_point(pch=21, color="grey30", alpha=0.5) +
+g <- ggplot(data %>% filter(comm_name%in%top20spp & depth_fa<100), aes(x=depth_fa, y=ratio)) +
+  facet_wrap(~factor(comm_name, levels=top20spp), ncol=5, scales="free_x") +
+  geom_point(pch=21, color="grey60", alpha=0.5, size=0.7) +
+  geom_smooth(fill="grey30", color="black", alpha=0.7, lwd=0.5) +
   # Horizontal line
   geom_hline(yintercept=1, linetype="dotted") +
   # Labels
-  labs(x="Depth (fathoms)", y="Bycatch ratio\n(bycatch / halibut catch)", title="CDFW gillnet observer data") +
+  labs(x="Depth (fathoms)", y="Catch ratio\n(discards / halibut landings)", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
   # Theme
@@ -217,19 +226,19 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=bottom_depth_fa, y=rat
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_cdfw_gillnet_obs_bycatch_ratio_by_depth.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_ratio_by_depth_southern_large.png"),
        width=6.5, height=5.5, units="in", dpi=600)
 
 
 # Bycatch ratio by day of year
 g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=date_dummy, y=ratio)) +
-  facet_wrap(~factor(comm_name, levels=top20spp), ncol=5, scales="free_y") +
-  geom_smooth(fill="grey80", color="black") +
-  geom_point(pch=21, color="grey30", alpha=0.5) +
+  facet_wrap(~factor(comm_name, levels=top20spp), ncol=5) +
+  geom_point(pch=21, color="grey60", alpha=0.5, size=0.7) +
+  geom_smooth(fill="grey30", color="black", alpha=0.7, lwd=0.5) +
   # Horizontal line
   geom_hline(yintercept=1, linetype="dotted") +
   # Labels
-  labs(x="Day of year", y="Bycatch ratio\n(bycatch / halibut catch)") +
+  labs(x="Day of year", y="Catch ratio\n(discards / halibut landings)", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
   scale_x_date(date_breaks = "2 months", date_labels =  "%b") +
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
@@ -239,6 +248,7 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=date_dummy, y=ratio)) 
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_cdfw_gillnet_obs_bycatch_ratio_by_date.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_ratio_by_date_southern_large.png"),
        width=6.5, height=5.5, units="in", dpi=600)
+
 
--- data/swfsc_obs/Step4_analyze_swfsc_obs_data_discards_dead.R
+++ data/swfsc_obs/Step4_analyze_swfsc_obs_data_discards_dead.R
@@ -26,11 +26,29 @@ source("code/helper_functions.R")
 # Build data
 ################################################################################
 
+# Port key
+# Determine whether ports are N/S of Pt Arguello
+port_key_big <- wcfish::ports
+port_key <- tibble(port=sort(unique(c(data_meta$port_depart, data_meta$port_return)))) %>%
+  mutate(port_match=recode(port,
+                           "Newport"="Newport Beach",
+                           "Port San Luis"="Avila/Port San Luis")) %>%
+  left_join(port_key_big %>% select(port, lat_dd, long_dd), by=c("port_match"="port")) %>%
+  mutate(region=ifelse(lat_dd>=34.6, "Northern", "Southern"))
+southern_ports <- port_key %>%
+  filter(region=="Southern") %>%
+  pull(port)
+
+
 # Build data
 data <- data_obs %>%
   # Add set meta data
-  left_join(data_meta %>% select(set_id, target1_spp, date_haul1, haul_lat_dd, haul_long_dd, haul_depth_fa), by="set_id") %>%
-  rename(lat_dd=haul_lat_dd, long_dd=haul_long_dd, depth_fa=haul_depth_fa, date=date_haul1) %>%
+  left_join(data_meta %>% select(set_id, target1_spp, season, date_haul1, port_depart, port_return,  haul_depth_fa), by="set_id") %>%
+  rename(depth_fa=haul_depth_fa, date=date_haul1) %>%
+  # Reduce to post-1994 data
+  filter(season>=1994) %>%
+  # Reduce to SOUTHERN fishery
+  filter(port_depart %in% southern_ports & port_return %in% southern_ports) %>%
   # Reduce to sets targeting CA halibut
   filter(target1_spp=="Halibut, California") %>%
   # Add year
@@ -44,14 +62,13 @@ data <- data_obs %>%
   ungroup() %>%
   select(-halibut_yn) %>%
   # Calculate bycatch ratios
+  mutate(discards_n=n_returned_dead) %>%
   group_by(set_id) %>%
   mutate(halibut_n=n_kept[comm_name=="California halibut"],
-         ratio=n_kept/halibut_n) %>%
+         ratio=discards_n/halibut_n) %>%
   ungroup() %>%
   # Filter to useable surveys
   filter(halibut_n>0 & ratio>0) %>%
-  # Remove halibut
-  filter(comm_name!="California halibut") %>%
   # Add date dummy
   mutate(date_dummy=paste("2020", lubridate::month(date), lubridate::day(date), sep="-") %>% lubridate::ymd(.))
 
@@ -67,24 +84,27 @@ freeR::complete(data)
 # Number of sets
 nsets_tot <- n_distinct(data$set_id)
 
-# Build stats
+# Determine species order
 stats <- data %>%
   group_by(comm_name) %>%
   summarize(nsets=n_distinct(set_id),
-            psets=nsets/nsets_tot) %>%
-  ungroup() %>%
+            psets=nsets/nsets_tot,
+            ratio_med=median(ratio)) %>%
   arrange(desc(psets))
 
 # Top 20 species
 top20spp <- stats$comm_name[1:20]
+top40spp <- stats$comm_name[1:40]
 
 
-
-# Plot data - bycatch species
+# Plot data
 ################################################################################
 
+# Bycatch species
+#######################################
+
 # Theme
-theme1 <-  theme(axis.text=element_text(size=5),
+theme2 <-  theme(axis.text=element_text(size=6),
                  axis.title=element_text(size=7),
                  axis.title.y=element_blank(),
                  plot.title=element_text(size=8),
@@ -97,33 +117,34 @@ theme1 <-  theme(axis.text=element_text(size=5),
                  # Legend
                  legend.background = element_rect(fill=alpha('blue', 0)))
 
-# Plot data
-g1 <- ggplot(stats, aes(y=factor(comm_name, levels=comm_name), x=psets)) +
-  # Boxplots
+
+# Plot bycatch ratio boxplots
+g1 <- ggplot(stats %>% filter(comm_name %in% top40spp), aes(x=psets, y=factor(comm_name, levels=stats$comm_name))) +
   geom_bar(stat="identity") +
+  # Reference line
+  geom_hline(yintercept=20.5, linetype="dashed") +
   # Labels
-  labs(x="Bycatch occurence\n(percent of gillnet sets)", y="", tag="A", title="SWFSC gillnet observer data") +
+  labs(x="Catch occurrence\n(percent of gill net sets)", y="", tag="A", title="SWFSC gill net observer data\nsouthern large-mesh fishery") +
   # Axis
   scale_x_continuous(labels=scales::percent) +
   # Theme
-  theme_bw() + theme1
+  theme_bw() + theme2
 g1
 
-# Plot data
-g2 <- ggplot(data, aes(y=factor(comm_name, levels=stats$comm_name), x=ratio)) +
-  # Boxplots
-  geom_boxplot(fill="grey90", lwd=0.2, outlier.size = 0.3) +
+# Plot bycatch ratio boxplots
+g2 <- ggplot(data %>% filter(comm_name %in% top40spp), aes(x=ratio, y=factor(comm_name, levels=top40spp))) +
+  geom_boxplot(lwd=0.2, outlier.size = 0.5, color="grey30", fill="grey90") +
   # Reference line
+  geom_hline(yintercept=20.5, linetype="dashed") +
+  # Vertical line
   geom_vline(xintercept = 1) +
   # Labels
-  labs(x="Bycatch ratio\n(bycatch / halibut catch)", y="", tag="B", title="  ") +
+  labs(x="Catch ratio\n(dead discards / halibut landings)", y="", tag="B", title=" \n ") +
   # Axis
-  scale_x_continuous(trans="log10",
-                     breaks=c(0.1, 1, 10, 100),
-                     labels=c("0.1", "1", "10", "100")) +
+  scale_x_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
   # Theme
-  theme_bw() + theme1 +
-  theme(axis.text.y=element_blank())
+  theme_bw() + theme2 +
+  theme(axis.text.y = element_blank())
 g2
 
 # Merge plots
@@ -131,16 +152,15 @@ g <- gridExtra::grid.arrange(g1, g2, nrow=1, widths=c(0.55, 0.45))
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_swfsc_gillnet_obs_bycatch_ratio_by_species.png"),
-       width=6.5, height=8.5, units="in", dpi=600)
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_dead_ratio_by_species_southern_large.png"),
+       width=6.5, height=6.5, units="in", dpi=600)
 
 
-
-# Plot data - bycatch over time
-################################################################################
+# Bycatch over time
+#######################################
 
 # Theme
-theme2 <- theme(axis.text=element_text(size=7),
+theme1 <- theme(axis.text=element_text(size=7),
                 axis.title=element_text(size=8),
                 strip.text=element_text(size=7),
                 plot.title=element_text(size=8),
@@ -159,20 +179,23 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=year, y=ratio, group=y
   # Reference line
   geom_hline(yintercept=1) +
   # Labels
-  labs(y="Bycatch ratio\n(bycatch / halibut catch)", x="Year", title="SWFSC gillnet observer data") +
+  labs(y="Catch ratio\n(dead discards / halibut landings)", x="Year", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
+  scale_x_continuous(breaks = seq(1995,2020,5), lim=c(1993,2020)) +
   # Theme
-  theme_bw() + theme2 +
+  theme_bw() + theme1 +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_swfsc_gillnet_obs_bycatch_ratio_over_time.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_dead_ratio_over_time_southern_large.png"),
        width=6.5, height=5, units="in", dpi=600)
 
-# Plot data - bycatch by driver
-################################################################################
+
+
+# Bycatch by driver
+#######################################
 
 # Theme
 theme3 <- theme(axis.text=element_text(size=7),
@@ -188,14 +211,14 @@ theme3 <- theme(axis.text=element_text(size=7),
                 legend.background = element_rect(fill=alpha('blue', 0)))
 
 # Bycatch ratio by depth
-g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=depth_fa, y=ratio)) +
+g <- ggplot(data %>% filter(comm_name%in%top20spp & depth_fa<100), aes(x=depth_fa, y=ratio)) +
   facet_wrap(~factor(comm_name, levels=top20spp), ncol=5, scales="free_x") +
   geom_point(pch=21, color="grey60", alpha=0.5, size=0.7) +
   geom_smooth(fill="grey30", color="black", alpha=0.7, lwd=0.5) +
   # Horizontal line
   geom_hline(yintercept=1, linetype="dotted") +
   # Labels
-  labs(x="Depth (fathoms)", y="Bycatch ratio\n(bycatch / halibut catch)", title="SWFSC gillnet observer data") +
+  labs(x="Depth (fathoms)", y="Catch ratio\n(dead discards / halibut landings)", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
   # Theme
@@ -203,7 +226,7 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=depth_fa, y=ratio)) +
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_swfsc_gillnet_obs_bycatch_ratio_by_depth.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_dead_ratio_by_depth_southern_large.png"),
        width=6.5, height=5.5, units="in", dpi=600)
 
 
@@ -215,7 +238,7 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=date_dummy, y=ratio)) 
   # Horizontal line
   geom_hline(yintercept=1, linetype="dotted") +
   # Labels
-  labs(x="Day of year", y="Bycatch ratio\n(bycatch / halibut catch)", title="SWFSC gillnet observer data") +
+  labs(x="Day of year", y="Catch ratio\n(dead discards / halibut landings)", title="SWFSC gill net observer data - southern large-mesh fishery") +
   # Axis
   scale_x_date(date_breaks = "2 months", date_labels =  "%b") +
   scale_y_continuous(trans="log10", breaks=c(0.01, 0.1, 1, 10, 100), labels=c("0.01", "0.1", "1", "10", "100")) +
@@ -225,11 +248,7 @@ g <- ggplot(data %>% filter(comm_name%in%top20spp), aes(x=date_dummy, y=ratio)) 
 g
 
 # Export plot
-ggsave(g, filename=file.path(plotdir, "FigX_swfsc_gillnet_obs_bycatch_ratio_by_date.png"),
+ggsave(g, filename=file.path(plotdir, "FigX_swfsc_observer_discards_dead_ratio_by_date_southern_large.png"),
        width=6.5, height=5.5, units="in", dpi=600)
-
-
-
-
 
 
